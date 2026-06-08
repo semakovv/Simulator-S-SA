346d0928c6ce28b9bd4843c084dd9e505693beda
semakovv <semakov.v@edu.narfu.ru>
2026-06-08 12:30:15 +0300

    Create button.py

---
+++ src/button.py
@@ -0,0 +1,26 @@
+import pygame
+
+#button class
+class buttonClass():
+	def __init__(self, x, y, image):
+		self.image = image
+		width = image.get_width()
+		height = image.get_height()
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+
+	def draw(self, surface):
+		action = False
+		mousePosition = pygame.mouse.get_pos()
+		if self.rect.collidepoint(mousePosition):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		surface.blit(self.image, (self.rect.x, self.rect.y))
+		
+		return action
