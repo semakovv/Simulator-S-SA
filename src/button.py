81c14ae3930c662c84d98fe355461a44a566863a
semakovv <semakov.v@edu.narfu.ru>
2026-06-12 02:03:43 +0300

    Update button.py

--- src/button.py
+++ src/button.py
@@ -1,8 +1,14 @@
 import pygame
 
 #button class
-class buttonClass():
+class link():
+	"""
+	
+	"""
 	def __init__(self, x, y, image):
+		"""
+		
+		"""
 		self.image = image
 		width = image.get_width()
 		height = image.get_height()
@@ -10,17 +16,35 @@ class buttonClass():
 		self.rect.topleft = (x, y)
 		self.clicked = False
 
-	def draw(self, surface):
+	def press(self, surface):
+		"""
+		
+		"""
 		action = False
 		mousePosition = pygame.mouse.get_pos()
 		if self.rect.collidepoint(mousePosition):
 			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
 				self.clicked = True
 				action = True
-
 		if pygame.mouse.get_pressed()[0] == 0:
 			self.clicked = False
-
 		surface.blit(self.image, (self.rect.x, self.rect.y))
-		
+		# print(action)
 		return action
+	
+	def move(self, surface):
+		"""
+		
+		"""
+		action = False
+		mousePosition = pygame.mouse.get_pos()
+		if self.rect.collidepoint(mousePosition):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+		surface.blit(self.image, (pygame.mouse.get_pos()[0], self.rect.y))
+		# print(action)
+		return action
+	
