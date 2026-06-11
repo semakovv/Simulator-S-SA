2e573fa506ab7069a34515a07a6c2c1fe3167dd3
semakovv <semakov.v@edu.narfu.ru>
2026-06-12 02:03:40 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,81 +1,113 @@
 import pygame
 import button
-
+import settings
+#
 pygame.init()
-
-winWidth = 1200
-winHeigth = 685
+#
+winWidth = 1920
+winHeigth = 1080
 window = pygame.display.set_mode((winWidth, winHeigth))
 pygame.display.set_caption("Simulator S&SA")
 backGround = pygame.image.load("assets/images/backGround.jpg")
 # pygame.display.set_icon("")
-
+#
 clock = pygame.time.Clock()
-
+#
 startButtonImage = pygame.image.load("assets/images/startButton.png")
+menuButtonImage = pygame.image.load("assets/images/menuButton.jpg")
+settingsButtonImage = pygame.image.load("assets/images/settingsButton.jpeg")
 exitButtonImage = pygame.image.load("assets/images/exitButton.png")
-volumeButtonImage = pygame.image.load("assets/images/volumeButton.png")
-startButton = button.buttonClass(100, 200, startButtonImage)
-exitButton = button.buttonClass(450, 200, exitButtonImage)
-volumeButton = button.buttonClass(1100, 0, volumeButtonImage)
+startButton = button.link(600, 400, startButtonImage)
+menuButton = button.link(1000, 0, menuButtonImage)
+settingsButton = button.link(1800, 0, settingsButtonImage)
+exitButton = button.link(1000, 400, exitButtonImage)
 
+volumeBackButtonImage = pygame.image.load("assets/images/backCounterButton.png")
+frameBackButtonImage = pygame.image.load("assets/images/backCounterButton.png")
+volumeFrontButtonImage = pygame.image.load("assets/images/frontCounterButton.png")
+frameFrontButtonImage = pygame.image.load("assets/images/frontCounterButton.png")
+volumeButton = button.link(600, 400, volumeFrontButtonImage)
+frameButton = button.link(600, 600, frameFrontButtonImage)
+#
+#
 soundMenu = pygame.mixer.Sound("assets/sounds/soundMenu.mp3")
 soundGame = pygame.mixer.Sound("assets/sounds/soundGame.mp3")
+#
+volume = settings.parameters()
+frame = settings.parameters()
+# resolution = settings.parameters()
 
-def redrawMenu():
-    window.blit(backGround, (0, 0))
-    # window.fill((255, 255, 255))
-    soundMenu.play(-1)
-
-    if startButton.draw(window):
-        return "start"
+class redraw():
+    """
     
-    if exitButton.draw(window):
-        return "exit"
+    """
+    def menu():
+        """
+        
+        """
+        window.blit(backGround, (0, 0))
+        # window.fill((255, 255, 255))
+        soundMenu.play(-1)
+        soundMenu.set_volume(volume.setVolume(0))
+        if startButton.press(window):
+            soundMenu.stop()
+            return "game"
+        if settingsButton.press(window):
+            return "setting"
+        if exitButton.press(window):
+            return "exit"
+        return "menu"
+
+    def game():
+        """
+        
+        """
+        window.blit(backGround, (0, 0))
+        # window.fill((255, 255, 255))
+        soundGame.play(-1)
+        soundGame.set_volume(volume.setVolume(0))
+        if menuButton.press(window):
+            soundGame.stop()
+            return "menu"
+        return "game"
     
-    if volumeButton.draw(window):
-        pass
+    def setting():
+        """
+        
+        """
+        window.blit(backGround, (0, 0))
+        window.blit(volumeBackButtonImage, (600, 400))
+        window.blit(frameBackButtonImage, (600, 400))
+        # window.fill((255, 255, 255))
+        if volumeButton.press(window):
+            return "setting"
+        if frameButton.press(window):
+            return "setting"
+        if exitButton.press(window):
+            return "exit"
+        return "setting"
 
-    return "menu"
-
-def redrawGame():
-    window.blit(backGround, (0, 0))
-    # window.fill((255, 255, 255))
-    soundGame.play(-1)
-
-    if volumeButton.draw(window):
-        pass
-
-    return "game"
-
+#
 run = True
 gameState = "menu"
-
-# mainloop
+#mainloop
 while run:
-    clock.tick(60)
-    
+    clock.tick(frame.setFrame(60))
     # print(gameState)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        
     if gameState == "menu":
-        gameState = redrawMenu()
-
-    if gameState == "start":
-        gameState = redrawGame()
-
+        gameState = redraw.menu()
+    if gameState == "game":
+        gameState = redraw.game()
+    if gameState == "setting":
+        gameState = redraw.setting()
     if gameState == "exit":
         run = False
-
     keys = pygame.key.get_pressed()
-
     if keys[pygame.K_ESCAPE]:
         run = False
-    
     pygame.display.update()
-
 
 pygame.quit()
