7e8cd6027731617e2bcad50735bd8bd55543f536
semakovv <semakov.v@edu.narfu.ru>
2026-06-12 21:53:08 +0300

    Update button.py

--- src/button.py
+++ src/button.py
@@ -1,19 +1,29 @@
 import pygame
+import settings
+
+movingButtonImage = pygame.image.load("assets/images/movingButton.png")
+holdingButtonImage = pygame.image.load("assets/images/holdingButton.png")
+
 
 #button class
 class link():
 	"""
 	
 	"""
-	def __init__(self, x, y, image):
+	def __init__(self, x, y, frontImage = movingButtonImage, backImage = holdingButtonImage):
 		"""
 		
 		"""
-		self.image = image
-		width = image.get_width()
-		height = image.get_height()
-		self.rect = self.image.get_rect()
-		self.rect.topleft = (x, y)
+		self.frontImage = frontImage
+		self.backImage = backImage
+		self.frontWidth = frontImage.get_width()
+		self.frontHeight = frontImage.get_height()
+		self.backWidth = backImage.get_width()
+		self.backHeight = backImage.get_height()
+		self.frontRect = self.frontImage.get_rect()
+		self.backRect = self.backImage.get_rect()
+		self.frontRect.topleft = (x, y)
+		self.backRect.topleft = (x, y)
 		self.clicked = False
 
 	def press(self, surface):
@@ -22,13 +32,14 @@ class link():
 		"""
 		action = False
 		mousePosition = pygame.mouse.get_pos()
-		if self.rect.collidepoint(mousePosition):
+		if self.frontRect.collidepoint(mousePosition):
 			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
 				self.clicked = True
 				action = True
 		if pygame.mouse.get_pressed()[0] == 0:
 			self.clicked = False
-		surface.blit(self.image, (self.rect.x, self.rect.y))
+		surface.blit(self.frontImage, (self.frontRect.x, self.frontRect.y))
+		# print(mousePosition)
 		# print(action)
 		return action
 	
@@ -36,15 +47,34 @@ class link():
 		"""
 		
 		"""
-		action = False
+		xMousePosition = pygame.mouse.get_pos()[0]
+		surface.blit(self.backImage, (self.backRect.x, self.backRect.y))
+		surface.blit(self.frontImage, (self.frontRect.x, self.frontRect.y))
 		mousePosition = pygame.mouse.get_pos()
-		if self.rect.collidepoint(mousePosition):
+		if self.frontRect.collidepoint(mousePosition):
 			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				# if link.press(surface):
 				self.clicked = True
-				action = True
+				# self.frontRect.x = surface.get_width() - xMousePosition + (self.frontWidth // 2) + self.backRect.x
+				print(self.frontRect.x)
+				self.frontRect.x = xMousePosition
+				surface.blit(self.frontImage, (self.frontRect.x, self.frontRect.y))
 		if pygame.mouse.get_pressed()[0] == 0:
 			self.clicked = False
-		surface.blit(self.image, (pygame.mouse.get_pos()[0], self.rect.y))
-		# print(action)
-		return action
-	
+
+
+		num = self.backWidth + self.frontWidth - self.frontRect.x
+		# print(num)
+		return num
+
+
+# volumeButton = link(600, 400, volumeFrontButtonImage)
+# frameButton = link(600, 600, frameFrontButtonImage)
+# volume = settings.parameters(volumeButton)
+# frame = settings.parameters(frameButton)
+#         window.blit(volumeBackButtonImage, (600, 400))
+#         window.blit(frameBackButtonImage, (600, 400))
+        # if volumeButton.press(window):
+        #     return "setting"
+        # if frameButton.press(window):
+        #     return "setting"
